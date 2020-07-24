943c7ec9d465581057dfa6bef60f90a081fc6362
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-07-24 12:34:34 +0300

    Add route for navigation between topics

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,7 @@
+from os import getenv
 from flask import Flask
 
 app = Flask(__name__)
-
+app.secret_key = 'secret'
+#print(getenv("SECRET_KEY"))
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,29 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from application import app
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/<topic>")
+def topic_page(topic):
+    name = ''
+    description = 'Conversations about '
+    if topic == 'bestsellers':
+        name = 'Bestsellers'
+        description += 'best-selling books'
+    elif topic == 'new_releases':
+        name = 'New Releases'
+        description += 'upcoming book releases'
+    elif topic == 'what_to_read':
+        name = 'What Should I Read Next?'
+        description += 'reading suggestion'
+    elif topic == 'authors':
+        name = 'Authors'
+        description += 'book authors'
+    else:
+        flash(f'Topic \'{topic}\' does not exist. Please choose one topic from the following list:')
+        return redirect(url_for("index"))
+
+    return render_template("topic.html", topic=topic, name=name, description=description)
+
