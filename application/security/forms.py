ef6845306769fe293f10d016360929838e3572b8
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-07-27 09:31:02 +0300

    Add validators to username field; add modified field to topics/models.py

--- application/security/forms.py
+++ application/security/forms.py
@@ -1,5 +1,7 @@
 from flask_security.forms import RegisterForm
 from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
 
 class ExtendedRegisterForm(RegisterForm):
-    username = StringField("Username")
+    username = StringField("Username", [DataRequired(), Length(min=4, message="Username must be at least 4 characters long.")])
--- application/topics/models.py
+++ application/topics/models.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from application import db
 
 class Message(db.Model):
@@ -7,10 +5,14 @@ class Message(db.Model):
     title = db.Column(db.String(140))
     msg_slug = db.Column(db.String(140), unique=True)
     body = db.Column(db.Text)
-    created = db.Column(db.DateTime, default=datetime.now())
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
 
 class Thread(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(140))
     thread_slug = db.Column(db.String(140), unique=True)
-    created = db.Column(db.DateTime, default=datetime.now())
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
