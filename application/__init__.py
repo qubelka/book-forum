d4b108e8b0212f92a19c29fb3a5af399587b14cd
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-07-28 15:47:05 +0300

    Add integration between flask admin and flask security

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 from .config import ConfigClass
+from .security.admin import AdminView, CustomAdminIndexView
 
 app = Flask(__name__)
 app.config.from_object(ConfigClass)
@@ -18,19 +18,19 @@ user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore, register_form=ExtendedRegisterForm)
 
 # Setup Flask-Admin
-admin = Admin(app)
-admin.add_view(ModelView(User, db.session))
-admin.add_view((ModelView(Role, db.session)))
-admin.add_view((ModelView(Thread, db.session)))
-admin.add_view((ModelView(Message, db.session)))
-admin.add_view((ModelView(Topic, db.session)))
+admin = Admin(app, index_view=CustomAdminIndexView())
+admin.add_view(AdminView(User, db.session))
+admin.add_view((AdminView(Role, db.session)))
+admin.add_view((AdminView(Thread, db.session)))
+admin.add_view((AdminView(Message, db.session)))
+admin.add_view((AdminView(Topic, db.session)))
 
 from application.topics.topics_bp import topics
 app.register_blueprint(topics, url_prefix='/topics')
 
 from application import routes
 
-''' uncomment to create db tables with test user
+''' uncomment to create db tables with test user and some pre-defined topics
 @app.before_first_request
 def create_user():
     db.drop_all()
---
+++ application/security/admin.py
@@ -0,0 +1,18 @@
+from flask import redirect, url_for, request
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import AdminIndexView
+from flask_security import current_user
+
+class AdminView(ModelView):
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login', next=request.url))
+
+class CustomAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login', next=request.url))
