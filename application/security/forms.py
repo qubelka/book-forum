e22ac49a8c304e5caa1af5e8bb79d53039a0c4b2
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-08-18 10:51:08 +0300

    Add error msgs for username and password

--- application/security/forms.py
+++ application/security/forms.py
@@ -1,6 +1,6 @@
 from flask_admin.contrib.sqla.validators import Unique
 from flask_security.forms import RegisterForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 from application import User, db
@@ -8,6 +8,10 @@ from application import User, db
 
 class ExtendedRegisterForm(RegisterForm):
     username = StringField('Username', [DataRequired(),
-                                        Length(min=4, message='Username should be at least 4 characters long.'),
+                                        Length(min=4, message='Username should be at least 4 characters long'),
+                                        Length(max=15, message='Username can be max 15 characters long'),
                                         Unique(model=User, column=User.username,
-                                               message='This username is already associated with an account.', db_session=db.session)])
+                                               message='This username is already associated with an account', db_session=db.session)])
+    password = PasswordField('Password', [DataRequired(),
+                                          Length(min=6, message='Password must be at least 6 characters long'),
+                                          Length(max=20, message='Password can be max 20 characters long')])
