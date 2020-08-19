2ed3288135aabd53002da48f1bf2e66e5ce82f14
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-08-19 09:42:30 +0300

    Add username validation

--- application/security/forms.py
+++ application/security/forms.py
@@ -1,17 +1,20 @@
 from flask_admin.contrib.sqla.validators import Unique
 from flask_security.forms import RegisterForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Regexp
 
 from application import User, db
-
 
 class ExtendedRegisterForm(RegisterForm):
     username = StringField('Username', [DataRequired(),
                                         Length(min=4, message='Username should be at least 4 characters long'),
                                         Length(max=15, message='Username can be max 15 characters long'),
                                         Unique(model=User, column=User.username,
-                                               message='This username is already associated with an account', db_session=db.session)])
+                                               message='This username is already associated with an account', db_session=db.session),
+                                        Regexp('^[a-zA-Z]+(_?[a-zA-Z0-9])*(_)?[a-zA-Z0-9]+$',
+                                               message='Username can contain only alphanumeric characters (includes underscore) '
+                                                       'and should start and end with a letter. No double underscores allowed.')])
+
     password = PasswordField('Password', [DataRequired(),
                                           Length(min=6, message='Password must be at least 6 characters long'),
                                           Length(max=20, message='Password can be max 20 characters long')])
