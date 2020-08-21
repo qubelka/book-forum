4d82eac75d7c595d93ae1fd149979b031cb3c5b4
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-08-21 15:24:55 +0300

    Fix problem with showing latest post

--- application/helper.py
+++ application/helper.py
@@ -1,31 +1,38 @@
 from flask_login import current_user
-from sqlalchemy import and_, or_
+from sqlalchemy import and_
 
 from application.topics.models import *
 
 def get_messages(topic_name):
+    message_list = Message.query.join(Thread).join(Topic). \
+        filter(and_(Topic.name == topic_name, ~Thread.secret_users.any())).all()
+
     if current_user.is_authenticated:
-        message_list = Message.query.join(Thread).join(Topic).\
-            filter(and_(Topic.name == topic_name, or_(~Thread.secret_users.any(), Thread.secret_users.contains(current_user)))).all()
-    else:
-        message_list = Message.query.join(Thread).join(Topic).\
-            filter(and_(Topic.name == topic_name, ~Thread.secret_users.any())).all()
+        secret_messages_with_access = Message.query.join(Thread).join(Topic).\
+            filter(and_(Topic.name == topic_name, Thread.secret_users.contains(current_user))).all()
+        message_list.extend(secret_messages_with_access)
+
     return message_list
 
 def get_latest_msg(topic_name):
+    latest_msg = Message.query.join(Thread).join(Topic). \
+        filter(and_(Topic.name == topic_name, ~Thread.secret_users.any())).order_by(Message.modified.desc()).first()
+
     if current_user.is_authenticated:
-        latest_msg = Message.query.join(Thread).join(Topic).\
-            filter(and_(Topic.name == topic_name, or_(~Thread.secret_users.any(), Thread.secret_users.contains(current_user)))).order_by(Message.modified.desc()).first()
-    else:
-        latest_msg = Message.query.join(Thread).join(Topic).\
-            filter(and_(Topic.name == topic_name, ~Thread.secret_users.any())).order_by(Message.modified.desc()).first()
+        latest_secret_msg = Message.query.join(Thread).join(Topic).\
+            filter(and_(Topic.name == topic_name, Thread.secret_users.contains(current_user))).order_by(Message.modified.desc()).first()
+
+        if latest_secret_msg and latest_msg:
+            if latest_secret_msg.modified > latest_msg.modified:
+                latest_msg = latest_secret_msg
+
     return latest_msg
 
 def get_threads(topic_name):
+    thread_list = Thread.query.join(Topic).filter(and_(Topic.name == topic_name, ~Thread.secret_users.any())).all()
+
     if current_user.is_authenticated:
-        thread_list = Thread.query.join(Topic).filter(and_(Topic.name == topic_name,
-                                                       or_(~Thread.secret_users.any(), Thread.secret_users.contains(current_user)))).all()
-    else:
-        thread_list = Thread.query.join(Topic).filter(and_(Topic.name == topic_name, ~Thread.secret_users.any())).all()
+        secret_with_access = Thread.query.join(Topic).filter(and_(Topic.name == topic_name, Thread.secret_users.contains(current_user))).all()
+        thread_list.extend(secret_with_access)
 
     return thread_list
